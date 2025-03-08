3cf78366d90b35d743d456d9bb9c4c54bd32c8a5
rxhul18 <rahulshah0728@gmail.com>
2025-03-09 01:58:25 +0530

    Remove unused card components and delete anime card

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,6 +1,4 @@
 export * from "./gh-stats";
 export * from "./animelink-card";
-export * from "./dc-status";
-export * from "./wakatime-stats";
 export * from "./links-card"
 export * from "./gh-link"
